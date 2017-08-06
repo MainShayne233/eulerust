ff54a250c7d6fb61cb8f1d8b6aeaad99829616c9
MainShayne233 <shaynetremblay@hotmail.com>
2017-08-05 23:46:35 -0400

    Break out math functions into math module

--- src/problem_3.rs
+++ src/problem_3.rs
@@ -1,52 +1,4 @@
-fn gcd(mut smaller_number: f64, mut greater_number: f64) -> f64 {
-    if smaller_number > greater_number {
-        return gcd(greater_number, smaller_number);
-    }
-    let mut remainder: f64 = greater_number % smaller_number;
-    while remainder != 0.0 {
-        greater_number = smaller_number;
-        smaller_number = remainder;
-        remainder      = greater_number % smaller_number;
-    }
-    return smaller_number;
-}
-
-fn is_prime(number: f64) -> bool {
-    if number <= 3.0 {
-        return number >= 2.0;
-    } else if number == 5.0 {
-        return true
-    } else if gcd(30.0, number) != 1.0 {
-        return false
-    }
-    let mut divisor: f64 = 7.0;
-    while divisor <= number.sqrt() {
-        if number % divisor          == 0.0 ||
-           number % (divisor + 4.0)  == 0.0 ||
-           number % (divisor + 6.0)  == 0.0 ||
-           number % (divisor + 10.0) == 0.0 ||
-           number % (divisor + 12.0) == 0.0 ||
-           number % (divisor + 16.0) == 0.0 ||
-           number % (divisor + 22.0) == 0.0 ||
-           number % (divisor + 24.0) == 0.0 {
-           return false;
-        }
-        divisor += 30.0;
-    }
-    return true;
-}
-
-fn next_prime(mut prime: f64) -> f64 {
-    if prime % 2.0 == 0.0 {
-        prime += 1.0;
-    }
-    loop {
-        prime += 2.0;
-        if is_prime(prime) {
-            return prime;
-        }
-    }
-}
+mod math;
 
 fn main() {
     let mut factor: f64 = 2.0;
@@ -55,7 +7,7 @@ fn main() {
         if number % factor == 0.0 {
             number /= factor;
         } else {
-            factor = next_prime(factor);
+            factor = math::next_prime(factor);
         }
     }
     println!("{}", factor);
